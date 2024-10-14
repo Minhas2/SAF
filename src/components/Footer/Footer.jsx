08d352ed64ab95690e92b932ccfe04905b7c9952
Minhas2 <103770686+Minhas2@users.noreply.github.com>
2024-10-14 02:03:25 -0700

    footer links added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <div className="bg-black">
       <div className="container py-3">
         <div className="d-flex text-white justify-content-center align-items-center">
-          <h3>Home</h3>
-          <h3 className="ps-3">Portfolio</h3>
+          <h3>
+            <Link to="/" className="text-white text-decoration-none">Home</Link>
+          </h3>
+          <h3 className="ps-3">
+            <Link to="/portfolio" className="text-white text-decoration-none">Portfolio</Link>
+          </h3>
         </div>
       </div>
     </div>
